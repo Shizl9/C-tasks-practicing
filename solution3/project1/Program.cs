73679b04029628bad898a24d58fc3e2a43777118
Sheika <sheika02x2@gmail.com>
2026-04-23 08:22:39 +0400

    add soluton3 & task1

---
+++ solution3/project1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 1 – Day Name Printer
+            Console.WriteLine("Enter number from(1-7):");
+            int day = int.Parse(Console.ReadLine());
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuseday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day number");
+                    break;
+
+            }
+        }
+    }
+}
