99551a68829fd81208858af7fe92793c23df8222
Sheika <sheika02x2@gmail.com>
2026-04-23 08:14:51 +0400

    add task2

--- solution2/project1/Program.cs
+++ solution2/project1/Program.cs
@@ -13,7 +13,18 @@ if (num > 0)
             { Console.WriteLine("nigative"); }
             else if (num == 0)
             { Console.WriteLine("zero"); }
-            else { Console.WriteLine("Invalid Input!"); }
+            else { Console.WriteLine("Invalid Input!");
+            }
+
+          
+            //*******Task2*********
+            Console.WriteLine("Enter number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            if (num1 % 2 == 0)
+            { Console.WriteLine("number is Even"); }
+            else
+            { Console.WriteLine("number is Odd"); }
+
         }
     }
 }
