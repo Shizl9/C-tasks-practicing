112cbc9acbdcdfa701d5224847f4bbf09cde08bf
Sheika <sheika02x2@gmail.com>
2026-04-23 08:00:04 +0400

    add solution1

---
+++ solution1/project1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number !");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("please enter second number !");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{num1}+{num2}={num1 + num2}");
+            Console.WriteLine($"{num1}-{num2}={num1 - num2}");
+            Console.WriteLine($"{num1}*{num2}={num1 * num2}");
+            Console.WriteLine($"{num1}/{num2}={num1 / num2}");
+        }
+    }
+}
