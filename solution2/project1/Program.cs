553ed2176a864b0b11264ccb6cab7b001f71a9c9
Sheika <sheika02x2@gmail.com>
2026-04-23 08:10:42 +0400

    add solution2

---
+++ solution2/project1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+Console.WriteLine("please Enter numper:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num > 0)
+            { Console.WriteLine("positive"); }
+            else if (num < 0)
+            { Console.WriteLine("nigative"); }
+            else if (num == 0)
+            { Console.WriteLine("zero"); }
+            else { Console.WriteLine("Invalid Input!"); }
+        }
+    }
+}
