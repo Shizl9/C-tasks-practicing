bdfaa87776e910a0523d4289b611b858a29810b4
Sheika <sheika02x2@gmail.com>
2026-04-29 10:58:15 +0400

    add queue

---
+++ solution10/project1/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            #region queue
+            Queue<string> names = new Queue<string>();
+
+            names.Enqueue("Yahya");
+            names.Enqueue("Ahmed");
+            names.Enqueue("Ali");
+            names.Enqueue("Omar");
+
+            int choice = 0;
+
+            while (choice != 5)
+            {
+                Console.WriteLine("\n--- MENU ---");
+                Console.WriteLine("1. Add String");
+                Console.WriteLine("2. Print First Element");
+                Console.WriteLine("3. Delete First Element");
+                Console.WriteLine("4. Show All Elements");
+                Console.WriteLine("5. Exit");
+
+                Console.Write("Enter your choice: ");
+                choice = int.Parse(Console.ReadLine());
+
+                if (choice == 1)
+                {
+                    Console.Write("Enter string: ");
+                    string input = Console.ReadLine();
+                    names.Enqueue(input);
+                }
+                else if (choice == 2)
+                {
+                    if (names.Count > 0)
+                        Console.WriteLine("First element: " + names.Peek());
+                    else
+                        Console.WriteLine("Queue is empty");
+                }
+                else if (choice == 3)
+                {
+                    if (names.Count > 0)
+                        Console.WriteLine("Deleted: " + names.Dequeue());
+                    else
+                        Console.WriteLine("Queue is empty");
+                }
+                else if (choice == 4)
+                {
+                    if (names.Count > 0)
+                    {
+                        Console.WriteLine("All elements:");
+                        foreach (var name in names)
+                            Console.WriteLine(name);
+                    }
+                    else
+                        Console.WriteLine("Queue is empty");
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice");
+                }
+            }
+            #endregion
+            #region dictionary
+             
+            #endregion
+        }
+    }
+}
