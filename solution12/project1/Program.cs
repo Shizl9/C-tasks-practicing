d89d863cb2312313c40450e04e1db9f7fd656688
Sheika <sheika02x2@gmail.com>
2026-05-03 09:16:39 +0400

    addd overloding function

---
+++ solution12/project1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine(sum(3, 4));
+            Console.WriteLine(sum(3, 4,6));
+            Console.WriteLine(sum(3, 5,7));
+        }
+         static int sum(int a, int b) 
+        {
+            return a + b;
+        }
+        static int sum(int a, int b, int c) 
+        {
+            return a + b + c;
+        }
+        static int sum(int a , int b , int c, int d)
+        { return a+ b+c+d;
+        }
+    }
+}
