b4eefebf91444c800c37d7af4ea1c4fc7b201aba
Sheika <sheika02x2@gmail.com>
2026-05-05 09:41:44 +0400

    add set& get

---
+++ solution15/project 1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace project_1
+{
+    internal class Program
+    {
+        class Car
+        {
+            public string color;
+            public int model_year;
+          
+            private int temp=10;
+                public int Speed
+            {
+                get { return temp; }
+                set
+                {
+                    if (value > 0)
+                        temp = value;
+                }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Car my_car = new Car();
+            Console.WriteLine("please Enter speed of car:");
+            my_car.Speed= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(my_car.Speed);
+        }
+    }
+}
